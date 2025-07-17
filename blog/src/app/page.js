9b8ca4dc6981fa42529d2137f4c2a5e1f0c18eba
LorenzFar <lorenzof.w.2023@scis.smu.edu.sg>
2025-07-17 13:15:18 +0700

    add logo for landing page

--- blog/src/app/page.js
+++ blog/src/app/page.js
@@ -1,6 +1,7 @@
 export default function Home(){
     return (
-        <div className="text-white">
+        <div className="grid min-h-screen place-content-center">
+             <img src="/Logo.svg" alt="Your Logo" />
         </div>
     );
 }
