40e5bc588f38092797c37b26ff776dcdbb60d99b
LorenzFar <lorenzof.w.2023@scis.smu.edu.sg>
2025-07-18 21:48:24 +0700

    add overlay on hover to hero grid

--- blog/src/app/layout.js
+++ blog/src/app/layout.js
@@ -3,6 +3,9 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link href="https://fonts.googleapis.com/css2?family=Syne:wght@400;600;700&display=swap" rel="stylesheet"></link>
+      </head>
       <body>
         <div className="god-rays">
             <div className="ray"></div>
--- blog/src/components/LandingHero.jsx
+++ blog/src/components/LandingHero.jsx
@@ -2,17 +2,61 @@ export default function LandingHero() {
   return (
     <div className="grid min-h-screen place-items-center">
       <div className="w-3/6">
+        <div className="grid grid-rows-8 grid-cols-13 gap-4">
+          
+          {/* Image 1 */}
+          <div className="relative group row-start-2 row-span-3 col-span-4 rounded-md overflow-hidden">
+            <img src="/Sinners.png" className="w-full h-full object-cover" />
+            <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-40 transition-opacity duration-300" />
 
-         <div className="grid grid-rows-8 grid-cols-12 gap-4">
-          {/* LEFT: Image */}
-          <img src="/Sinners.png" className="w-full h-full object-cover row-start-2 row-span-3 col-span-4 rounded-md" />
-          <img src="/F1.jpg" className="w-full h-full object-cover col-start-5 row-span-4 col-span-2 rounded-md" />
-          <img src="/F4.jpeg" className="w-full h-full object-cover row-start-3 col-start-7 row-span-2 col-span-3 rounded-md" />
-          <img src="/Superman.jpeg" className="w-full h-full object-cover row-start-5 col-start-2 row-span-3 col-span-6 rounded-md" />
-          <img src="/JWR.jpg" className="w-full h-full object-cover row-start-5 row-span-4 col-span-4 rounded-md" />
+            <p className="absolute top-8 left-5 text-white text-2xl font-extrabold opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+              SINNERS
+            </p>
+          </div>
 
-          {/* Right: Logo */}
-          <img src="/Logo.svg" alt="Your Logo" className="w-full h-full row-start-4 col-start-10 col-span-3" />
+          {/* Image 2 */}
+          <div className="relative group col-start-5 row-span-4 col-span-3 rounded-md overflow-hidden">
+            <img src="/F1.jpg" className="w-full h-full object-cover" />
+            <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-40 transition-opacity duration-300" />
+
+            <p className="absolute top-8 left-5 text-white text-lg font-extrabold opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+              F1: THE MOVIE
+            </p>
+          </div>
+
+          {/* Image 3 */}
+          <div className="relative group row-start-3 col-start-8 row-span-2 col-span-3 rounded-md overflow-hidden">
+            <img src="/F4.jpeg" className="w-full h-full object-cover" />
+            <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-40 transition-opacity duration-300" />
+
+            <p className="absolute top-8 left-5 text-white text-lg font-extrabold opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+              FANTASTIC 4
+            </p>
+          </div>
+
+          {/* Image 4 */}
+          <div className="relative group row-start-5 col-start-2 row-span-3 col-span-6 rounded-md overflow-hidden">
+            <img src="/Superman.jpg" className="w-full h-full object-cover" />
+            <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-40 transition-opacity duration-300" />
+
+            <p className="absolute top-8 left-5 text-white text-4xl font-extrabold opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+              SUPERMAN
+            </p>
+          </div>
+
+          {/* Image 5 */}
+          <div className="relative group row-start-5 row-span-4 col-span-5 rounded-md overflow-hidden">
+            <img src="/JWR.jpg" className="w-full h-full object-cover" />
+            <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-40 transition-opacity duration-300" />
+
+            <p className="absolute top-8 left-5 text-white text-lg font-extrabold opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+              JURASSIC WORLD: REBIRTH
+            </p>
+          </div>
+
+          {/* Logo (no overlay) */}
+          <img src="/Logo.svg" alt="Your Logo" className="w-full h-full row-start-4 col-start-11 col-span-3" />
+        
         </div>
       </div>
     </div>
