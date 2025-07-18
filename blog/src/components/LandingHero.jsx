8e558e61c9e2ac4896ed589477d3f0d709dd17fa
LorenzFar <lorenzof.w.2023@scis.smu.edu.sg>
2025-07-18 11:22:08 +0700

    fix grid positioning for hero grid

--- blog/src/components/LandingHero.jsx
+++ blog/src/components/LandingHero.jsx
@@ -1,19 +1,18 @@
 export default function LandingHero() {
   return (
     <div className="grid min-h-screen place-items-center">
-      <div className="grid grid-cols-3 items-center w-4/5 h-[80vh]">
-        {/* LEFT: Image Grid */}
-        <div className="grid col-span-2 grid-rows-8 grid-cols-8 gap-4 w-full h-full">
-          <img src="/F1.jpg" className="w-full h-full object-cover row-start-2 row-span-3 col-span-3 rounded-md" />
-          <img src="/F1.jpg" className="w-full h-full object-cover col-start-4 row-span-4 col-span-2 rounded-md" />
-          <img src="/Sinners.png" className="w-full h-full object-cover row-start-3 col-start-6 row-span-2 col-span-2 rounded-md" />
-          <img src="/Sinners.png" className="w-full h-full object-cover row-start-5 col-start-2 row-span-3 col-span-3 rounded-md" />
-          <img src="/JWR.jpg" className="w-full h-full object-cover row-start-5 row-span-4 col-span-4 rounded-md" />
-        </div>
+      <div className="w-3/6">
 
-        {/* RIGHT: Logo */}
-        <div className="flex ">
-          <img src="/Logo.svg" alt="Your Logo" />
+         <div className="grid grid-rows-8 grid-cols-12 gap-4">
+          {/* LEFT: Image */}
+          <img src="/Sinners.png" className="w-full h-full object-cover row-start-2 row-span-3 col-span-4 rounded-md" />
+          <img src="/F1.jpg" className="w-full h-full object-cover col-start-5 row-span-4 col-span-2 rounded-md" />
+          <img src="/F4.jpeg" className="w-full h-full object-cover row-start-3 col-start-7 row-span-2 col-span-3 rounded-md" />
+          <img src="/Superman.jpeg" className="w-full h-full object-cover row-start-5 col-start-2 row-span-3 col-span-6 rounded-md" />
+          <img src="/JWR.jpg" className="w-full h-full object-cover row-start-5 row-span-4 col-span-4 rounded-md" />
+
+          {/* Right: Logo */}
+          <img src="/Logo.svg" alt="Your Logo" className="w-full h-full row-start-4 col-start-10 col-span-3" />
         </div>
       </div>
     </div>
