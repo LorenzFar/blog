29ad39a35d7b51c91b994c25fe7769402ce33a24
LorenzFar <lorenzof.w.2023@scis.smu.edu.sg>
2025-07-19 09:38:11 +0700

    fix image hero text size + gaming page component

--- blog/src/components/ImageHero.jsx
+++ blog/src/components/ImageHero.jsx
@@ -2,10 +2,16 @@ export default function ImageHero({ title, imageURL, className = '' }) {
   return (
     <div className={`relative group rounded-md overflow-hidden ${className}`}>
       <img src={imageURL} alt={title} className="w-full h-full object-cover" />
+      
+      {/* Overlay with flex center alignment */}
       <div className="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-40 transition-opacity duration-300" />
-      <p className="absolute top-8 left-5 text-white text-2xl font-extrabold opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-        {title}
-      </p>
+
+      {/* Text with flexible positioning */}
+      <div className="absolute inset-0 flex place-items-center justify-start p-5 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+        <p className="text-white font-black text-[1vw]">
+            {title}
+        </p>
+      </div>
     </div>
   );
 }
